b5c0aa5c1860e506f303ac91d66613a935b13f46
Matheus Santos de Almeida <matheusalmeida@tokenlab.com.br>
2019-12-16 17:29:01 -0300

    link do tutorial hasmany

--- models/holidaysModel.ts
+++ models/holidaysModel.ts
@@ -1,6 +1,16 @@
 const db    = require('../config/db');
 const type  = db.Sequelize;
 
+//adicionar relacionamento hasmany
+//https://medium.com/@edtimmer/sequelize-associations-basics-bde90c0deeaa
+let calendarsModel = db.define('calendars',{
+    id:{
+        type: type.INTEGER,
+        primaryKey: true
+    },
+    Nome:type.STRING
+});
+
 let holidaysModel = db.define('holidays',{
     id:{
         type: type.INTEGER,
